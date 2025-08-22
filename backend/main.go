ab203b2ca26aeb3098a247125de7f58116512bba
Bradw91 <imbrad91@gmail.com>
2025-08-22 19:53:25 +0200

    defined initial endpoints and set up initial gin server

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	// Group Routes
+	workItemsApi := router.Group("/api/v1/workItems")
+	{
+		workItemsApi.GET("/", getWorkItems)
+		workItemsApi.POST("/", createWorkItem)
+		workItemsApi.PUT("/:id", updateWorkItem)
+		workItemsApi.DELETE("/:id", deleteWorkItem)
+	}
+
+	userApi := router.Group("/api/v1/users")
+	{
+		userApi.GET("/", getUsers)
+		userApi.POST("/", createUser)
+		userApi.PUT("/:id", updateUser)
+		userApi.DELETE("/:id", deleteUser)
+	}
+
+	router.Run(":8080")
 }
